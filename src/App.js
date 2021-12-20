c618a2b8ecdec336a8c26ec4202006709e5a5ab7
Juri <jasonluxie@gmail.com>
2021-12-19 18:44:06 -0600

    moved cards around, deciding on layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects"
-import Form from "./components/Form"
+// import Form from "./components/Form"
 import { CssBaseline } from "@mui/material";
 function App() {
     return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,7 @@ import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import "../styles/Projects.css";
 import dndTracker from "../images/1-dnd-tracker.gif";
-import noteTaker from "../images/2-note-taker.gif";
 import egsChecker from "../images/3-egs-checker.png";
-import empSys from "../images/4-emp-sys.png";
 import codeQuiz from "../images/6-code-quiz.png";
 import weatherTracker from "../images/7-weather-tracker.png";
 export default function Projects() {
@@ -19,22 +17,12 @@ export default function Projects() {
                 flexDirection: "column",
                 alignItems: "center",
                 px: 1,
-                py: 2
+                py: 2,
             }}
         >
             <Typography variant="h3" align="center" sx={{ pt: 2 }}>
-                My Projects
+                Some Projects
             </Typography>
-
-            <ModalCard
-                main="true"
-                title={`D&D Character Tracker`}
-                value="Dungeons and Dragons non-player character tracker for dungeon masters."
-                imageLocation={dndTracker}
-                imageAlt="GIF showing application functionality including login, character creation, randomization, and dice rolling."
-                link="https://github.com/jasonluxie/8-1-project-2"
-
-            />
             <Box
                 sx={{
                     display: "flex",
@@ -46,11 +34,11 @@ export default function Projects() {
             >
                 <ModalCard
                     main="false"
-                    title={`Note Taker`}
-                    value="Note taking application using MySQL database."
-                    imageLocation={noteTaker}
-                    imageAlt="GIF showing some notes being entered."
-                    link="https://github.com/jasonluxie/6-2-hw-note-taker"
+                    title={`D&D Character Tracker`}
+                    value="Dungeons and Dragons non-player character tracker for dungeon masters."
+                    imageLocation={dndTracker}
+                    imageAlt="GIF showing application functionality including login, character creation, randomization, and dice rolling."
+                    link="https://github.com/jasonluxie/8-1-project-2"
                 />
                 <ModalCard
                     main="false"
@@ -59,14 +47,6 @@ export default function Projects() {
                     imageLocation={egsChecker}
                     imageAlt="Landing page for application showing instructions."
                     link="https://github.com/jasonluxie/4-1-project-1"
-                />
-                <ModalCard
-                    main="false"
-                    title={`Employee Management System`}
-                    value="Console based MySQL database management system"
-                    imageLocation={empSys}
-                    imageAlt="Image of console showing database being accessed via CRUD."
-                    link="https://github.com/jasonluxie/7-1-hw-employee-management-system"
                 />
                 <ModalCard
                     main="false"
